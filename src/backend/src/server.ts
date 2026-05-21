826a0897dae75828c7f0e371deed83a5e987a0db
Funaki 41 <kauanfunaki.41@gmail.com>
2026-05-21 15:26:26 -0300

    server fixed for deploy

--- src/backend/src/server.ts
+++ src/backend/src/server.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use('/api', routes);
 
 if (process.env.NODE_ENV === 'production') {
-  const staticPath = path.join(__dirname, '../../frontend/dist');
+  const staticPath = path.join(__dirname, '../src/frontend/dist');
   app.use(express.static(staticPath));
   app.get('*', (_, res) => res.sendFile(path.join(staticPath, 'index.html')));
 }
